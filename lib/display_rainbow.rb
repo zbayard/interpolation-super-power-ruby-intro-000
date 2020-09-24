68438f8400812a553d005738c5ea055538ebb50b
Zach Bayard <zbayard@gmail.com>
2020-09-24 18:16:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,2 @@
-def display_rainbow(['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'])
-  print "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
-end
+def display_rainbow(colors = ['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'])
+  

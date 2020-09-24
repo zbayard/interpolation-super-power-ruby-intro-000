0b87c75f8b6a8e461b85ca74cdcfc061090f52a2
Zach Bayard <zbayard@gmail.com>
2020-09-24 17:58:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'])
-  print "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end

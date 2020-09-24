340ce86513616cc9048ba7a29eccb2a1cb6e3280
Zach Bayard <zbayard@gmail.com>
2020-09-24 18:21:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,9 @@
 def display_rainbow(colors = ['red' , 'orange' , 'yellow' , 'green' , 'blue' , 'indigo' , 'violet'])
+puts colors[0]
+puts colors[1]
+puts colors[2]
+puts colors[3]
+puts colors[4]
+puts colors[5]
+puts colors[6]
 end
-  
